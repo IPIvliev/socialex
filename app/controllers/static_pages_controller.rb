4df36fac99fa03968fa816645e40794248af9bd7
IPIvliev <rusroad@gmail.com>
2013-09-23 14:29:34 +0400

    Smth do

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,8 @@
 class StaticPagesController < ApplicationController
   def index
 
+
+
 @client = Twitter::Client.new(
   :consumer_key => "8SmX3dJKoSbQq8hCOMkNAg",
   :consumer_secret => "HvdfJDBiat9zi21n7Cw0vE2hVp4jIsZHkm1JoZfk",
@@ -10,7 +12,7 @@ class StaticPagesController < ApplicationController
   :oauth_token_secret => "kchFu4R9VHVLgMDCkSbV5y8XxJ5F0PGiaQ3LNRi3jJA"
 )
 
-@graph = Koala::Facebook::API.new('CAABbDjcti5UBAAmE0Vty2gdhi8OvYI0y9mhwEmuLguJFSvXEUi8YmUfkVUqQ4eLdTfyMWfJ79Jr2B0Pi1ZCkGcAEYwEZA6wNqtRwuTj7r8rTlrbcZBSz5fbmpjmmMVe2FdYdZARq5ZCKTE9qZCyohFKhGcrRuZAdVGvDlHVi1RhmZCyzSZBhDHK0XYoA8PIG0xO0es3hrv0b0AgZDZD')
+@graph = Koala::Facebook::API.new('CAABbDjcti5UBAI3sL6ZCbokm6F3fmiEf0t44LtEMAk8Ixku1kTHsebpXL5uIPPGH9PRDWqXTD0W45ThQLImCykJ89OhD247kbzGZC4v47Glp7DlGXSEvO0bfZC9UYPpRgZCYBtPGOX58Bw3iDkjnSgDVkg9QeZANhZAfa9LzWnZBrjlpMT2NqJ9M8dFd7Ru6KA4HzfmZBN5AKQZDZD')
 @profile = @graph.get_object("me")
 @test1 = @graph.get_object("100003478442020")
 @test2 = @graph.get_object("rshlegel")
@@ -19,4 +21,8 @@ class StaticPagesController < ApplicationController
 @test5 = @graph.get_object("aleksandr.gaponenko.9")
 @test6 = @graph.get_object("margarita.dragile")
   end
+
+   def show
+    @user = User.find(params[:id])
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Socialex::Application.routes.draw do
   get "static_pages/index"
 
+  resources :users
+
 root to: "static_pages#index"
 
 end
